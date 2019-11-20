da374f899db83793d7bb8ebb000ca66f1caf7718
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-19 20:59:19 -0600

    adding react router

--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -7,7 +7,7 @@ import './homepage.styles.scss';
 const HomePage = () => (
     <div className='homepage'>
         <Directory />
-    </div>    
+    </div>
 );
 
-export default HomePage;
+export default HomePage; 
