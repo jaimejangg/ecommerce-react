d5621a18b0a5a8222711216eb86fb1d60b10ed35
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-20 22:01:37 -0600

    building header component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/Shop.component';
+import Header from './components/header/Header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>  
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
