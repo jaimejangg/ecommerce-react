83af79b65e9a89b9d5e7ffca58a9c92c5518750e
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-20 21:25:28 -0600

    collection preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/Homepage.component';
+import ShopPage from './pages/shop/Shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>  
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/Shop.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/previe-collection/Preview-collection.component';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+    render() {
+        const { collections } = this.state;
+
+        return (
+            <div className='shop-page'>
+                {
+                    collections.map(({ id, ...otherCollectionsProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionsProps} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
+
