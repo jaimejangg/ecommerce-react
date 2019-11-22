824a96325beda82665f7f44255fee02b70968b6d
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-22 12:12:56 -0600

    login form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/Shop.component';
 import Header from './components/header/Header.component';
+import SignInAndSignUpPage from './pages/sign-in-sign-up/Sign-in-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>  
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/Sign-in-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-sign-up.styles.scss';
+
+import SignIn from '../../components/sign-in/Sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;
