8d480c73c6d772e37c278e2a0467f355973c6124
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-11-29 11:46:12 -0600

    authentication complete

--- src/pages/sign-in-sign-up/Sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/Sign-in-sign-up.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './sign-in-sign-up.styles.scss';
 
 import SignIn from '../../components/sign-in/Sign-in.component';
+import SignUp from '../../components/sign-up/Sign-up-component';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
